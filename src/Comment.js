93a12f843175fc8c9ca34fab3793459194acc6bd
Alice Genkin <agenkin1@gmail.com>
2021-08-05 13:48:14 -0400

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,18 @@
 //your code here
+//import React ( Component ) from 'React'
+//use class x exntends component {} syntax
+//export the class so it can be used in other files
+//import the class in BlogPost
+
+import React, { Component } from 'react'
+
+export default class Comment extends Component {
+  render()  {
+    return (
+      <div className="comment">
+      {this.props.commentText}
+      </div>
+    )
+  }
+}
+
